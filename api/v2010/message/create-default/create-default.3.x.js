ed66d35367a5f826350a440047ed8aab3bd24edb
faresjhon <47419294+faresjhon@users.noreply.github.com>
2019-02-07 16:16:52 +0300

    changed two lines

--- api/v2010/message/create-default/create-default.3.x.js
+++ api/v2010/message/create-default/create-default.3.x.js
@@ -1,10 +1,10 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
-const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-const authToken = 'your_auth_token';
+const accountSid = 'AC5d5ae3dbd5dfc5463a828dc680bdd25e';
+const authToken = '0f0dbed1ae384fabd152b0e00ba0c1ae';
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
-      .create({from: '+15017122661', body: 'body', to: '+15558675310'})
+      .create({from: '+815032051199', body: 'body', to: '+97333978297'})
       .then(message => console.log(message.sid))
       .done();
